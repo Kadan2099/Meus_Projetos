0486f40b189c87c34d42970249529271bcb98000
João Victor Silva <jvitoriano0309@gmail.com>
2025-11-05 12:12:00 -0300

    Implement task registration prototype in Cadastro.c
    Initial prototype for task registration with fields for name, summary, and days.

--- Cornojob/Cadastro.c
+++ Cornojob/Cadastro.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+/*
+Parte designada para cadastrar em arquivo as informações de tarefas, vamos iniciar o protótipo com;
+Nome - [Char:20]
+Resumo da Atividade - [Char:200]
+Dias - [Int]
+
+*/
+
+
+int main(){
+    char Nome_da_atividade[20];
+    char Resumo_da_Atividade[200];
+    int Dias;
+    
+    system("pause");
+    return 0;
+}
